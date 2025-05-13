ec10ce7b17fe7606d1895f84893042d68c251d5f
harun demircioğlu <harun.demircioglu02@gmail.com>
2025-05-13 16:29:16 +0300

    some front update

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -1,9 +1,4 @@
 @extends('layouts.app')
 
-@section('title')
-    Login
-@endsection
-
 @section('content')
-    <h1 class="text-red-600">deneme</h1>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,23 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <title>@yield('title')</title>
-    @vite('resources/css/app.css')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'App')</title>
+
+    @stack('styles')
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
-    <div class="container">
+<body class="bg-gray-100 text-gray-900 min-h-screen">
+
+    <div class="container mx-auto py-8">
         @yield('content')
     </div>
+
+    @stack('scripts')
 </body>
 
 </html>
