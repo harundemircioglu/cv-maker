9b3cf51c3ae8f2cbd58bf4e407647f27a9478bd6
harun demircioğlu <harun.demircioglu02@gmail.com>
2025-07-15 15:55:32 +0300

    fonts & colors & tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#449399",
+                secondary: "#313C4E",
+                secondaryGray: "#989DA6",
+            },
+            fontFamily: {
+                ubuntuRegular: ["ubuntu-regular", "sans-serif"],
+                ubuntuMedium: ["ubuntu-medium", "sans-serif"],
+                ubuntuLight: ["ubuntu-light", "sans-serif"],
+                ubuntuBold: ["ubuntu-bold", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
